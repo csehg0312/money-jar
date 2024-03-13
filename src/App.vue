6b752643bdf6ef585acc5385154f08df67e013a5
csehg <csehgabor451@gmail.com>
2024-03-13 21:25:12 +0100

    Trasnported from one project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import MoneyJars from './components/MoneyJars.vue';
+import VirtualStore from './components/VirtualStore.vue';
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <MoneyJars />
+    <VirtualStore />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -28,3 +25,13 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<script>
+  export default {
+    components: {
+      MoneyJars,
+      VirtualStore,
+    },
+  };
+</script>
+
---
+++ src/components/VirtualStore.vue
@@ -0,0 +1,28 @@
+<!-- VirtualStore.vue -->
+<template>
+    <div>
+      <div v-for="item in items" :key="item.id" @click="handleItemPurchase(item)">
+        {{ item.name }} - ${{ item.price }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        items: [
+          { id: 1, name: 'Toy', price: 10 },
+          { id: 2, name: 'Book', price: 5 },
+          // Add more items as needed
+        ],
+      };
+    },
+    methods: {
+      handleItemPurchase(item) {
+        // Implement logic to handle item purchase
+      },
+    },
+  };
+  </script>
+  
