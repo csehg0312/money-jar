8cb450a16812b4615c32f00ac68ca173883bca25
csehg <csehgabor451@gmail.com>
2024-03-13 21:43:14 +0100

    Vite config modified

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['sweetalert2/dist/sweetalert2.min.css']
+    }
+  }
 })
