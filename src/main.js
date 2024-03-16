050faf351a7a8355d249d744497e875003565f3d
csehg <csehgabor451@gmail.com>
2024-03-16 22:57:02 +0100

    Merging to main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 // import 'sweetalert2/dist/sweetalert2.min.css';
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('src/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  }
+
 import App from './App.vue'
 
 const app = createApp(App);
