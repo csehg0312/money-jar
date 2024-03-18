319079c2893050a2ce81486b8dd40f445b7359e2
csehg <csehgabor451@gmail.com>
2024-03-18 19:24:43 +0100

    moved service-worker to top level

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import VueSweetalert2 from 'vue-sweetalert2'
 // import 'sweetalert2/dist/sweetalert2.min.css';
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./src/sw.js').then(registration => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(error => {
       console.log('ServiceWorker registration failed: ', error);
