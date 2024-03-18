756d43cfbc849e2f4e2f411758ea041c486a7609
csehg <csehgabor451@gmail.com>
2024-03-18 19:34:36 +0100

    service-worker loads after page load

--- src/main.js
+++ src/main.js
@@ -4,13 +4,6 @@ import VueSweetalert2 from 'vue-sweetalert2'
 // import './src/sw.js'
 // import 'sweetalert2/dist/sweetalert2.min.css';
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(error => {
-      console.log('ServiceWorker registration failed: ', error);
-    });
-  }
 
 import App from './App.vue'
 
@@ -19,3 +12,11 @@ const app = createApp(App)
 app.use(VueSweetalert2)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  }
