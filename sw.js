474fe4dd490fd4d3e2cb6e48506cf53eca3b4607
csehg <csehgabor451@gmail.com>
2024-03-18 19:43:18 +0100

    serice-worker

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'sweetalert';
+const links = [
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.6/dist/sweetalert2.min.css',
+  // add more links here as needed
+];
+
+
+
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        const promises = links.map(link => cache.add(new Request(link)));
+        return Promise.all(promises);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = event.request.url;
+  for (const link of links) {
+    if (url.endsWith(link)) {
+      event.respondWith(
+        caches.match(event.request)
+          .then(response => {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          })
+      );
+      break;
+    }
+  }
+});
